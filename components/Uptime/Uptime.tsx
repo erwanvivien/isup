98be4af41bd483e557322903877b5858efeafacc
Erwan <erwan.vivien@epita.fr>
2022-11-01 18:52:58 +0100

    fix: first render of isup

--- components/Uptime/Uptime.tsx
+++ components/Uptime/Uptime.tsx
@@ -3,10 +3,51 @@ import styles from "./Uptime.module.css";
 import { Suspense, useEffect, useState } from "react";
 import { RetrieveData, RetrieveResponse } from "../../pages/api/retrieve";
 import Service from "./Service";
+import { Grid, Loader, SimpleGrid, Title } from "@mantine/core";
+
+type Stats = Record<
+  string,
+  { total: number; failed: number; succeeded: number }
+>;
+
+const formatStats = (
+  data: RetrieveData["statuses"]
+): [Record<string, Stats>, Stats] => {
+  const serviceEntries: [string, Stats][] = Object.entries(data).map(
+    ([serviceName, commandStatuses]) => [
+      serviceName,
+      commandStatuses.reduce((acc, curr) => {
+        acc[curr.commandName] ??= { total: 0, failed: 0, succeeded: 0 };
+
+        acc[curr.commandName].total++;
+        if (curr.retcode !== 0) acc[curr.commandName].failed++;
+        else acc[curr.commandName].succeeded++;
+
+        return acc;
+      }, {} as Stats),
+    ],
+    {}
+  );
+
+  const statEntries = serviceEntries.reduce((acc, [serviceName, curr]) => {
+    acc[serviceName] ??= { total: 0, failed: 0, succeeded: 0 };
+
+    Object.values(curr).forEach(({ total, failed, succeeded }) => {
+      acc[serviceName].total += total;
+      acc[serviceName].failed += failed;
+      acc[serviceName].succeeded += succeeded;
+    });
+
+    return acc;
+  }, {} as Stats);
+
+  return [Object.fromEntries(serviceEntries), statEntries];
+};
 
 const Uptime: React.FC = () => {
   const [services, setServices] = useState<RetrieveData["services"]>([]);
-  const [statuses, setStatuses] = useState<RetrieveData["statuses"]>({});
+  const [serviceStats, setStatsService] = useState<Record<string, Stats>>({});
+  const [stats, setStats] = useState<Stats>({});
 
   useEffect(() => {
     const controllerAbort = new AbortController();
@@ -19,8 +60,11 @@ const Uptime: React.FC = () => {
 
       if (!resp || !resp.success) return;
 
+      const [serviceStats, stats] = formatStats(resp.data.statuses);
+
       setServices(resp.data.services);
-      setStatuses(resp.data.statuses);
+      setStatsService(serviceStats);
+      setStats(stats);
     };
 
     fetchServices();
@@ -33,15 +77,26 @@ const Uptime: React.FC = () => {
   }, []);
 
   return (
-    <Suspense>
+    <Suspense fallback={<Loader />}>
+      <Title order={2}>Test</Title>
       <div className={styles.container}>
-        {services.map((service) => (
-          <Service
-            key={service.name}
-            service={service}
-            statuses={statuses[service.name]}
-          />
-        ))}
+        <SimpleGrid cols={3}>
+          {services.map((service, i) => (
+            <Service
+              key={service.name}
+              title={service.name}
+              total={stats[service.name]?.total ?? 0}
+              completed={stats[service.name]?.succeeded ?? 0}
+              stats={[
+                {
+                  label: "Failed",
+                  value: stats[service.name]?.failed ?? 0,
+                },
+                { label: "Interval", value: service.interval },
+              ]}
+            />
+          ))}
+        </SimpleGrid>
       </div>
     </Suspense>
   );
