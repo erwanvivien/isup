d6fc36f2744c02854fbb4c4f3431859febccd9a2
Erwan <erwan.vivien@epita.fr>
2022-11-01 16:28:17 +0100

    feat: starting using Mantine UI3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles={true}
+        withNormalizeCSS={true}
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "dark",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
