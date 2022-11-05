02af63a80fe7a9cb43ad3d768f50ce035b02bfa9
Erwan <erwan.vivien@epita.fr>
2022-11-05 23:01:25 +0100

    fix: added OG components in head

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,27 @@ export default function Home() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
         <link rel="shortcut icon" href="/favicon.png" />
         <link rel="apple-touch-icon" href="favicon.png" />
+
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <meta name="theme-color" content="var(--background)" />
+        <meta property="og:title" content="IsUp" />
+        <meta
+          property="og:image"
+          content="https://isup.xiaojiba.dev/logo_small.png"
+        />
+        <meta property="og:image:alt" content="IsUp" />
+        <meta property="og:type" content="website" />
+        <meta
+          name="description"
+          content="Always keep an eye on your services. Fully configurable. Fully scallable."
+        />
+        <meta
+          property="og:description"
+          content="Always keep an eye on your services. Fully configurable. Fully scallable."
+        />
+        <meta name="author" content="Erwan VIVIEN (github: erwanvivien)" />
+        <meta name="keywords" content="uptime" />
       </Head>
 
       <main className={styles.main}>
